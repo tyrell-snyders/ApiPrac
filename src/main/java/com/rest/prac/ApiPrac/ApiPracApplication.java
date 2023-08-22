6dbadc9a9b07062556eeed4e0353136ef0feb5a0
tyrell.snyders <tyrell.snyders@gmail.com>
2023-08-22 16:02:07 +0200

    first commit

---
+++ src/main/java/com/rest/prac/ApiPrac/ApiPracApplication.java
@@ -0,0 +1,13 @@
+package com.rest.prac.ApiPrac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPracApplication.class, args);
+	}
+
+}
