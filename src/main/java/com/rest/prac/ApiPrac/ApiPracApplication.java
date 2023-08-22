42aa051039d074dc3f4cac3da3ca8ad9bc950d30
tyrell.snyders <tyrell.snyders@gmail.com>
2023-08-22 17:42:26 +0200

    Convert Java Object to JSON Object

--- src/main/java/com/rest/prac/ApiPrac/ApiPracApplication.java
+++ src/main/java/com/rest/prac/ApiPrac/ApiPracApplication.java
@@ -1,5 +1,7 @@
 package com.rest.prac.ApiPrac;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,9 +16,25 @@ public class ApiPracApplication {
 	}
 
 	//
-	@GetMapping("/") //Maps the the method to /
-	public String greet() {
-		return "Hello";
+	@GetMapping("/") //Maps the the method to / | @GetMapping is used for GET requests 
+	public GreetResponse greet() {
+		return new GreetResponse(
+				"Hello",
+				List.of("C++", "Java", "JavaScript", "Rust"),
+				new Person("Tyrell", 21)
+			); //This will return a JSON object
+			//The object is the response that is returned
+
+		/* 
+			The reason why it returns a JSON object
+			is because The annotation @RestController indicates that the class is a controller,
+			and that all the methods in the marked class will return a JSON object
+		*/
 	}
+
+	record Person(String name, int age) {}
+
+	//Return a JSON Object
+	record GreetResponse(String res, List<String> programmingLangs, Person person) {} //res is the key
 
 }
