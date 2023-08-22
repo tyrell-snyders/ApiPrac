472eed26a066b663c1ce5c8d74a66afc4ceb2fd0
tyrell.snyders <tyrell.snyders@gmail.com>
2023-08-22 17:04:05 +0200

    Created Rest Controller and Mapped a path("/")

--- src/main/java/com/rest/prac/ApiPrac/ApiPracApplication.java
+++ src/main/java/com/rest/prac/ApiPrac/ApiPracApplication.java
@@ -2,12 +2,21 @@ package com.rest.prac.ApiPrac;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController //Indicates that this is a rest controller
 public class ApiPracApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiPracApplication.class, args);
+	}
+
+	//
+	@GetMapping("/") //Maps the the method to /
+	public String greet() {
+		return "Hello";
 	}
 
 }
