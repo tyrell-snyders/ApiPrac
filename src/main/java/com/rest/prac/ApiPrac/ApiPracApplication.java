8ba8512343ab323451c0f00320fcc880bf1c3099
tyrell.snyders <tyrell.snyders@gmail.com>
2023-08-25 15:32:47 +0200

    Cleaned the code and created Customer Entity to create database rows

--- src/main/java/com/rest/prac/ApiPrac/ApiPracApplication.java
+++ src/main/java/com/rest/prac/ApiPrac/ApiPracApplication.java
@@ -4,8 +4,8 @@ import java.util.List;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+ import org.springframework.web.bind.annotation.RestController;
 
 // import com.rest.prac.ApiPrac.Models.Customer;
 
@@ -15,27 +15,5 @@ public class ApiPracApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiPracApplication.class, args);
 	}
-
-	//
-	// @GetMapping("/") //Maps the the method to / | @GetMapping is used for GET requests 
-	// public GreetResponse greet() {
-	// 	return new GreetResponse(
-	// 			"Hello",
-	// 			List.of("C++", "Java", "JavaScript", "Rust"),
-	// 			new Person("Tyrell", 21)
-	// 		); //This will return a JSON object
-	// 		//The object is the response that is returned
-
-	// 	/* 
-	// 		The reason why it returns a JSON object
-	// 		is because The annotation @RestController indicates that the class is a controller,
-	// 		and that all the methods in the marked class will return a JSON object
-	// 	*/
-	// }
-
-	// record Person(String name, int age) {}
-
-	// //Return a JSON Object
-	// record GreetResponse(String res, List<String> programmingLangs, Person person) {} //res is the key
 
 }
--- src/main/java/com/rest/prac/ApiPrac/Entity/Customer.java
+++ src/main/java/com/rest/prac/ApiPrac/Entity/Customer.java
@@ -1,8 +1,24 @@
-package com.rest.prac.ApiPrac.Models;
+package com.rest.prac.ApiPrac.Entity;
 
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+
+@Entity
 public class Customer {
+    @Id
+    @SequenceGenerator(
+            name = "customer_id_sequence",
+            sequenceName = "customer_id_sequence"
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "customer_id_sequence"
+    )
     private Integer id;
     private String name;
     private String email;
