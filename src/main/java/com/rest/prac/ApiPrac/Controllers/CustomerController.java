5ec0d2dc80b3762204462fcbd82934b22a57f207
tyrell.snyders <tyrell.snyders@gmail.com>
2023-08-25 20:03:43 +0200

    Created AddCustomer Post request

--- src/main/java/com/rest/prac/ApiPrac/Controllers/CustomerController.java
+++ src/main/java/com/rest/prac/ApiPrac/Controllers/CustomerController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import com.rest.prac.ApiPrac.Entity.Customer;
 import com.rest.prac.ApiPrac.Repositories.CustomerRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +21,20 @@ public class CustomerController {
 
     @GetMapping() //Uses the default endpoint: "api/v1/customers"
     public List<Customer> getCustomer() {
+        //Read and return the customers from the DB
         return customerRepository.findAll();
+    }
+
+    record NewCustomerRequest(String name, String email, Integer age) {}
+
+    @PostMapping
+    public void addCustomer(@RequestBody NewCustomerRequest request) { //Receives JSON object from data
+        Customer customer = new Customer();
+
+        customer.setName(request.name);
+        customer.setEmail(request.email);
+        customer.setAge(request.age);
+
+        customerRepository.save(customer); //Inserts into the customer table
     }
 }
